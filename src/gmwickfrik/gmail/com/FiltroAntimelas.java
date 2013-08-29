90e1992f3b22b3873dd64ba2b81bf7df78f05df5
Wickfrik <carlos_coloborde@yahoo.es>
2013-08-29 21:06:14 +0200

    Subida del repositorio

---
+++ src/gmwickfrik/gmail/com/FiltroAntimelas.java
@@ -0,0 +1,15 @@
+package gmwickfrik.gmail.com;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FiltroAntimelas {
+
+	public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+	
+}
